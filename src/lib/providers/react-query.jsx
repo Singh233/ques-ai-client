2c9b6576f23ce6bb45c2af038e62244e38b0f54e
Sanambir Singh <Sanambir123@gmail.com>
2025-04-26 23:10:27 +0530

    refact: change react-query file extension from tsx to jsx

--- src/lib/providers/react-query.jsx
+++ src/lib/providers/react-query.jsx
@@ -4,11 +4,7 @@ import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export default function ReactQueryProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function ReactQueryProvider({ children }) {
   // Create a client
   const [queryClient] = useState(() => new QueryClient());
 

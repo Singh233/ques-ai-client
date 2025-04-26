9188725716752434748c44ae24b46520e9ce6b34
Sanambir Singh <Sanambir123@gmail.com>
2025-04-26 18:13:07 +0530

    feat: integrate React Query with provider components and update dependencies

--- src/lib/providers/providers.jsx
+++ src/lib/providers/providers.jsx
@@ -1,11 +1,13 @@
-import React from "react";
 import ReduxProvider from "../redux/provider";
+import ReactQueryProvider from "./react-query";
 
 // Wrapper component to provide all providers to the app
 function Providers({ children }) {
   return (
     <>
-      <ReduxProvider>{children}</ReduxProvider>
+      <ReactQueryProvider>
+        <ReduxProvider>{children}</ReduxProvider>
+      </ReactQueryProvider>
     </>
   );
 }

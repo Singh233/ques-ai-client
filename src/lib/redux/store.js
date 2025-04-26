ee9663cc945758e2cdad388643998fdfdfa95689
Sanambir Singh <Sanambir123@gmail.com>
2025-04-26 23:11:23 +0530

    feat: implement authentication slice with user profile fetching and cookie management

--- src/lib/redux/store.js
+++ src/lib/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
 
 import counterReducer from "./features/counterSlice";
+import authReducer from "./features/authSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       counter: counterReducer,
+      auth: authReducer,
     },
     devTools: process.env.NODE_ENV !== "production",
   });

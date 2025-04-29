9fefa891299189eabd2bcac1a580d6b1fde28152
Sanambir Singh <Sanambir123@gmail.com>
2025-04-29 19:30:27 +0530

    impr: make / page responsive and overall improvements

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,31 @@
 import Logo from "~/components/Logo/Logo";
 import styles from "./page.module.scss";
 import Auth from "~/components/Auth/Auth";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles["page"]}>
       <div className={styles["page__left"]}>
+        <div className={styles["page__left__background"]}>
+          <Image
+            src={`/images/waves.png`}
+            alt="Create Project"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
         <div className={styles["page__left__info-container"]}>
           <div className={styles["page__left__info-container__logo"]}>
             <Logo size="lg" variant="text-white" />
           </div>
           <h1 className={styles["page__left__info-container__podcast-heading"]}>
-            Your Podcast, Your Way, Powered by AI
+            Your podcast, <br></br>
+            will no longer <br></br>
+            be just a hobby.
           </h1>
           <p className={styles["page__left__info-container__tagline"]}>
-            Transform your podcast production workflow with AI. Transcribe,
-            edit, and publish podcasts with ease. Get started today and
-            experience the future of podcasting.
+            Supercharge Your Distribution <br></br> using our AI assistant!
           </p>
         </div>
       </div>
